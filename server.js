31dfa49a763d23bb2606f593de0ea5caf9b3eefb
Khalid Shawki <khalid.i.shawki@gmail.com>
2022-07-15 02:53:54 +0200

    Building Server.js

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express'),
+      cors = require('cors'),
+      bodyParser = require('body-parser'); 
 
 // Start up an instance of app
+const app = express(); 
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const PORT = process.env.port || 9000;
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT} ..`));
+
+// Get Data from API
+app.use("/addData", addData);
+app.use("/sendData", sendData);
+
+
+
+// Helper Function
+const addData = (request, response) => {
+  let data = request.body;
+  console.log(`Server side data ${data}`);
+
+  projectData["date"] = data.date; 
+  projectData["temp"] = data.temp; 
+  projectData["feeling"] = data.feeling; 
+
+  response.send(projectData);
+}
+
+sendData = (request, response) => {
+  request.send(projectData);
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,18 @@
 /* Global Variables */
+const API_KEY = 'd049ed25ecfb2a1d010c09c7c67a6402&units=metric'
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let date = new Date();
+let newDate = `${date.getMonth()}.${date.getDate()}.${date.getFullYear()}`
+
+// Get the ZipCode, and Feeling from User
+const zipCode = document.querySelector('#zip').value;
+const feeling = document.querySelector('#feeling').value;
+const generate = document.querySelector('#generate').value;
+
+// generate.addEventListener('click', )
+
+
+const URL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},&appid=${API_KEY}`
+
+//  Helper Function
